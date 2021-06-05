df37164da904754ae61e2c6f47edc7c50c280a59
Thalia De Mattos <tgdmattos@gmail.com>
2021-06-05 19:46:26 -0400

    last few adjustments

--- routes/api.js
+++ routes/api.js
@@ -1,14 +1,6 @@
 const router = require("express").Router();
-const Workout = require("../models/workout.js");
-router.post("/api/workouts", (req, res) => {
-  Workout.create({})
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
+const Workout = require("../models");
+
 router.put("/api/workouts/:id", ({ body, params }, res) => {
   Workout.findByIdAndUpdate(
     params.id,
