002f6c1461aeab6de524f90f7814b5b2abf14077
Thalia De Mattos <tgdmattos@gmail.com>
2021-06-07 15:03:09 -0400

    tweaked serevr.js

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 3000;
-
-const db = require("./models");
+const PORT = 3000;
 
 const app = express();
 
@@ -14,14 +12,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
-app.use(require("./controllers"));
 
-mongoose.connect(process.env.MONGODB_URI|| "mongodb://localhost/workout", {
+mongoose.connect('mongodb://localhost/workout', {
   useNewUrlParser: true,
+  useFindAndModify: false,
   useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
 });
+
+app.use(require('./routes/api.js'));
+app.use(require('./routes/view.js'));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
