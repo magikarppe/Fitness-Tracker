21f08d9499a64a4fa93a1ee9ad23728c5e905574
Thalia De Mattos <tgdmattos@gmail.com>
2021-06-07 17:14:49 -0400

    troubleshooting

--- seeders/seed.js
+++ seeders/seed.js
@@ -9,7 +9,7 @@ mongoose.connect("mongodb://localhost/Fitness-Tracker", {
 
 let workoutSeed = [
   {
-    day: new Date(new Date ().setDate(new Date().getDate() - 9),
+    day: new Date(new Date ().setDate(new Date().getDate() - 9)),
     exercises: [
       {
         type: 'resistance',
@@ -22,8 +22,8 @@ let workoutSeed = [
     ],
   },
   {
-    day: new Date(new Date().setDate(new Date().getDate() - 8),
-    exercises [
+    day: new Date(new Date().setDate(new Date().getDate() - 8)),
+    exercises: [
       {
         type: "resistance",
         name: "Lateral Pull",
